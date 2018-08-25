81deeea5d28979f5f75899e7b5af9d068e6a1c64
Roger Hatherall <roger.hatherall@gmail.com>
2018-08-25 17:14:07 +0100

    Display map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
+
 class Map extends Component {
+
   render(){
+
+    const NeighborhoodMap =withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter = { { lat: 51.5558, lng: -1.7797 } }
+        defaultZoom = { 10 }
+      >
+      </GoogleMap>
+   ));
+
     return(
-      <div>Map Template</div>
+      <div>
+        <NeighborhoodMap
+          containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
+          mapElement={ <div style={{ height: `100%` }} /> }
+        />
+      </div>
     );
   }
 };
