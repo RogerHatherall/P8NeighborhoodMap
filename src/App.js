97a848c610705a9f2ceebd499a4d93086a162995
Roger Hatherall <roger.hatherall@gmail.com>
2018-08-26 17:02:07 +0100

    Added Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header/>
         <Map/>
+        <Footer/>
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+
+	render() {
+
+		return(
+			<footer className="app-footer" >
+				<h2 className= "app-footer-heading" tabIndex={0}>
+					Site built by Roger Hatherall using React Google Maps and Foursquare
+				</h2>
+      </footer>
+		)
+	}
+}
+
+export default Footer
